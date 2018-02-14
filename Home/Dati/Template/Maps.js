dfd774b83fb4ccb97dc2e273e48e876e5cdf6429
Andrea Grandi <MotAtlas@gmail.com>
2018-02-14 09:47:20 +0100

    Ricaricamento data layer
    Ora ricarica il data layer e basta, rendendo inutile il ricaricare l'intera mappa

--- Home/Dati/Template/Maps.js
+++ Home/Dati/Template/Maps.js
@@ -1,49 +1,34 @@
-function initMap(flayer) {
+function initMap() {
   //Inizializzazione della var contenete la mappa, con definizione del centro, del livello di zoom e della div su cui far partire la cosa
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.476780, lng: 9.259794},
     zoom: 14
   });
   //Inizializzazione del layer di poligoni derivato dalla fusion table, con definizione del tipo, della tabella, e di vari elementi di stile
-  var layer;
-  if(flayer != undefined){
-    layer = flayer;
-  }
-  else{
-      layer = new google.maps.FusionTablesLayer({
-      query: {
-        select: 'geometry',
-        from: '17LYcPq8I-54Yzozqnq6xUus2RyQsPU1fkUH5KKqP'
-      },
-      templateId: 2,
-      styles: [{
-        polygonOptions: {
-          fillColor: '#00FF00',
-          fillOpacity: 0.3
-        }
-      }, {
-        where: 'POP_2010 > 15',
-        polygonOptions: {
-          fillColor: '#0000FF'
-        }
-      }, {
-        where: 'POP_2010 < 15',
-        polygonOptions: {
-          fillOpacity: 1.0
-        }
-      }]
-      });
-    }
+  var layer = new google.maps.FusionTablesLayer(layerSelector(1));
   //Caricamento del layer sopra alla mappa inizializzata in precedenza
   layer.setMap(map);
-}
-function initLayer(flayer){
-  
+  var mod1 = document.getElementById('mod1');
+  var mod2 = document.getElementById('mod2');
+  var mod3 = document.getElementById('mod3');
+  //add event listener
+  mod1.addEventListener('click', function(event) {
+    layer.setOptions(layerSelector(1));
+    console.log("mod1");
+  });
+  mod2.addEventListener('click', function(event) {
+    layer.setOptions(layerSelector(2));
+    console.log("mod2");
+  });
+  mod3.addEventListener('click', function(event) {
+    layer.setOptions(layerSelector(3));
+    console.log("mod3");
+  });
 }
 function layerSelector(mode){
   var layer;
   if(mode == 1){
-    layer = new google.maps.FusionTablesLayer({
+    layer = {
     query: {
       select: 'geometry',
       from: '17LYcPq8I-54Yzozqnq6xUus2RyQsPU1fkUH5KKqP'
@@ -65,10 +50,10 @@ function layerSelector(mode){
         fillOpacity: 1.0
       }
     }]
-    });
+    };
   }
   else if(mode == 2){
-    layer = new google.maps.FusionTablesLayer({
+    layer = {
     query: {
       select: 'geometry',
       from: '17LYcPq8I-54Yzozqnq6xUus2RyQsPU1fkUH5KKqP'
@@ -90,10 +75,10 @@ function layerSelector(mode){
         fillOpacity: 0.1
       }
     }]
-    });
+    };
   }
   else{
-    layer = new google.maps.FusionTablesLayer({
+    layer = {
     query: {
       select: 'geometry',
       from: '17LYcPq8I-54Yzozqnq6xUus2RyQsPU1fkUH5KKqP'
@@ -115,7 +100,7 @@ function layerSelector(mode){
         fillOpacity: 0.1
       }
     }]
-    });
+    };
   }
-  initMap(layer);
+  return layer;
 }
